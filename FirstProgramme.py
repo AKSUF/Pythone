0ddaa0bdcd71668901558bb9a2fea9234b5b9d88
AKSUF <abu053125@gmail.com>
2023-05-02 01:31:49 +0600

    pythone if,else,if else

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,17 +1,14 @@
-#comparision operator
-print(3<2)
-print(2>3)
-print(3<=2)
-print(2>=3)
-print(3==2)
-print(3==3)
-print(3!=2)
-#or operator
-print(2>3 or 2>1)
-#and operator
-print(2>3 and 2>1)
-#not operator
-print(not 2>3)
+age =1
+#age =17
+if age>=18:
+    print("you are an adult")
+    print("you can vot")
+elif age<18 and age>3:
+    print("you are in school")
+else:
+    print("you are a child")
+
+print("thank you")
 
 
 
