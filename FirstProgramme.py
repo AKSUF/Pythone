85c6134941fa7b2756c9c739b38859cda7d2b35a
AKSUF <abu053125@gmail.com>
2023-05-02 00:16:20 +0600

    type error problem

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,2 +1,12 @@
-name=input ("What is your name?")
-print("Hello"+name)
+old_age=input("enter your old age:")
+
+new_age=int(old_age)+2
+print(new_age)
+number=18
+print(float(number))
+
+first=input("enter first number:")
+second=input("enter second number:")
+sum=int(first)+int(second)
+print(sum)
+print("the sum is"+str(sum))
