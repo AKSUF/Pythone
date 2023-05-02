0135d2b5da2600fc43856c9da564f691adaf3f17
AKSUF <abu053125@gmail.com>
2023-05-02 22:45:22 +0600

    boolean in pythone

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,23 +1,25 @@
-x="hello World"
-print(type(x))
-x=20
-print(type(x))
-x=20.5
-print(type(x))
-1j
-print(type(x))
-x=["apple", "banana", "cherry"]
-print(type(x))
-x = ("apple", "banana", "cherry")
-print(type(x))
-x = range(6)
-print(type(x))
-x = {"name" : "John", "age" : 36}
-print(type(x))
-x = frozenset({"apple", "banana", "cherry"})
-print(type(x))
+# a=200
+# b=33
+# if b>a:
+#     print("b is greater than a")
+# else:
+#     print("b is not greater than a")
 
+#Checking if an element exists in a list using the in keyword
+# my_list=[1,2,3,4,5]
+# result=2 in my_list
+# print(result)
+# result=6 in my_list
+# print(result)
+x = 5
+y = 10
+z = 15
 
+result = x < y and y < z
+print(result)   # True
+
+result = x < y or y > z
+print(result)   # True
 
 
 
