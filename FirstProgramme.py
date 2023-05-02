260f4253ad87df6116791cddef12b4e1caac282c
AKSUF <abu053125@gmail.com>
2023-05-02 11:37:30 +0600

    datatype in pythone

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,8 +1,21 @@
-import math
-from math import *
-print(sqrt(16))
-print(dir(math))
-
+x="hello World"
+print(type(x))
+x=20
+print(type(x))
+x=20.5
+print(type(x))
+1j
+print(type(x))
+x=["apple", "banana", "cherry"]
+print(type(x))
+x = ("apple", "banana", "cherry")
+print(type(x))
+x = range(6)
+print(type(x))
+x = {"name" : "John", "age" : 36}
+print(type(x))
+x = frozenset({"apple", "banana", "cherry"})
+print(type(x))
 
 
 
