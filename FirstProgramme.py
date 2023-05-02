c9acbb5ad3c825621acf73c850bbf9445a66f3b7
AKSUF <abu053125@gmail.com>
2023-05-02 07:35:31 +0600

    continue & break keyword

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,20 +1,13 @@
-marks=[95,98,97]
-print(marks)
-print(marks[-1])
-print(marks[1])
-print(marks[1:3])
-marks.append(99)
-marks.insert(5,45)
-print(marks)
-for score in marks:
-    print(score)
-i=0
-while i<len(marks):
-    print(marks[i])
-    i=i+1
-marks.clear()
-print(marks)
+students=["ram","shyam","kishan","radha","radhika"]
 
+# for students in students:
+#     if students=="radha":
+#         break;
+#     print(students)
+for students in students:
+    if students=="kishan":
+        continue;
+    print(students)
 
 
 
