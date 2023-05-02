134cfd89ac912d0955f935e2ffa26e22f9140634
AKSUF <abu053125@gmail.com>
2023-05-02 07:54:18 +0600

    set in pythone

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,6 +1,10 @@
-marks=(95,98,97,97,97)
-print(marks.count(97))
-print(marks.index(97))
+#[]-list,()-tupple,{}-Set
+person="ram","shyam","abbhi"
+print(person)
+marks={95,98,97,97,97}
+print(marks)
+for score in marks:
+    print(score)
 
 
 
