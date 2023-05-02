f37adbafd475c00062ed47f2f3fc2b470dc36aea
AKSUF <abu053125@gmail.com>
2023-05-02 07:27:15 +0600

    array and related

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,17 +1,20 @@
-numbers=range(5)
-print(numbers)
-#while loop
-# i=1
-# while i<=10:
-#     print(i*"*")
-#     i=i+1
-# while i>=0:
-#     print(i*"*")
-#     i=i-1
+marks=[95,98,97]
+print(marks)
+print(marks[-1])
+print(marks[1])
+print(marks[1:3])
+marks.append(99)
+marks.insert(5,45)
+print(marks)
+for score in marks:
+    print(score)
+i=0
+while i<len(marks):
+    print(marks[i])
+    i=i+1
+marks.clear()
+print(marks)
 
-#for loop
-for i in range(5):
-    print(i)
 
 
 
