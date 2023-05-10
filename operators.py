f6101a25050517207281a0d17685711de9a647cf
AKSUF <abu053125@gmail.com>
2023-05-10 10:55:31 +0600

    string ,different types,slicing in string

--- operators.py
+++ operators.py
@@ -1,3 +1,71 @@
+# a = "Hello, World!"
+# print(a[7])
+
+# my_string = "Hello,World!"
+# print(my_string[0:5])   # Output: Hello
+
+# my_string = "Hello, World!"
+# print(my_string[1:6])   # Output: !
+
+
+# my_string="Hello ,World"
+# for character in my_string:
+#     print(character)
+
+
+# my_friend="khusboo"
+# for character in my_friend:
+#     print(character)
+
+# my_string = "Hello, World!"
+# for i in range(len(my_string)):
+#     print(my_string[i])
+
+
+# my_string = "Hello, World!"
+
+# # Get the first 5 characters of the string
+# print(my_string[0:5])   # Output: "Hello"
+
+# # Get the characters from index 7 to the end of the string
+# print(my_string[7:])   # Output: "World!"
+
+# Get every other character in the string
+# print(my_string[::2])   # Output: "Hlo ol!"
+
+# # Get the characters from index 3 to index 8, skipping every other character
+# print(my_string[3:8:2])   # Output: "l,"
+#
+# # Reverse the string
+# print(my_string[::-1])   # Output: "!dlroW ,olleH"
+
+
+# String Length
+a="Hello,World"
+print(len(a))
+
+
+# my_string = "Hello, World!"
+# if "World" in my_string:
+#     print("Yes, 'World' is present in the string.")
+# else:
+#     print("No, 'World' is not present in the string.")
+
+
+my_friend="Khusboo,Azima"
+if"Khusboo" in my_friend:
+    print("Yes","This persion is your friend")
+else:
+    print("No","This persion is not your friend")
+
+# kjjhhg
+my_string = "Hello, World!"
+if my_string.find("World") != -1:
+    print("Yes, 'World' is present in the string.")
+else:
+    print("No, 'World' is not present in the string.")
+
+
 # slicing in pythone
 # my_string = "Hello, World!"
 # print(my_string[:3])  # Output: "Hello"
