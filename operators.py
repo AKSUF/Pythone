905004d5dc0359833f334b359ddbace602f645bb
AKSUF <abu053125@gmail.com>
2023-05-10 07:45:41 +0600

    specify a type on to a variable

--- operators.py
+++ operators.py
@@ -1,66 +1,36 @@
-# x=1
-# y=7575757
-# z=-5665555545
-# print(type(x))
-# print(type(y))
-# print(type(z))
-#
-
-# x=1.10
-# y=1.0
-# z=-35.59
-# print(type(x))
-# print(type(y))
-# print(type(z))
-
-# x=35e3
-# y=12E4
-# z=-87.7e100
-# print(type(x))
-# print(type(y))
-# print(type(z))
-# #complex
-# x = 3+5j
-# y = 5j
-# z = -5j
+# x = float(1)
+# y = float(2.8)
+# z = float("3")
+# w = float("4.2")
 # print(x)
 # print(y)
 # print(z)
+# print(w)
 
-# x=1+78
-# y=2.8
-# z=1j
-# #convert from int to float
-# a=float(x)
-# c=complex(x)
-# print(c)
-#
-#
-# z1 = 2 + 3j
-# z2 = 4 + 5j
-#
-# # Addition
-# z3 = z1 + z2
-# print(z3)   # Output: (6+8j)
-#
-# # Subtraction
-# z4 = z2 - z1
-# print(z4)   # Output: (2+2j)
-#
-# # Multiplication
-# z5 = z1 * z2
-# print(z5)   # Output: (-7+22j)
-#
-# # Division
-# z6 = z2 / z1
-# print(z6)   # Output: (2.2+0.4j)
 
-# import random
-# print(random.randrange(1,10))
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
 
-import random
-mylist = ["apple", "banana", "cherry", "durian"]
-print(random.choice(mylist))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
