e24a15cb2934f695f885881a1f3377cdf94a116b
AKSUF <abu053125@gmail.com>
2023-05-10 06:52:32 +0600

    how different types of variable is working

---
+++ operators.py
@@ -0,0 +1,122 @@
+# fruits=["apple","banana","cherry"]
+# x,y,z=fruits
+# print(x)
+# print(y)
+# print(z)
+# #ghghfghffg
+# my_tuple = (1, 2, 3)
+#
+# x, y, z = my_tuple
+#
+# print(type(x))   # 1
+# print(y)   # 2
+# print(z)   # 3
+# #unpacking
+# my_string = "Hello"
+#
+# a, b, c, d, e = my_string
+#
+# print(a)   # H
+# print(b)   # e
+# print(c)   # l
+# print(d)   # l
+# print(e)   # o
+#
+# my_hello="Khusboo"
+# a,b,c,d,e,f,g=my_hello
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+
+
+# my_list=[1,2,3,4,5]
+# x,y,*rest=my_list
+# print(x)
+# print(y)
+# print(rest*5)
+#
+# x="awesome"
+# def myfunc():
+#     print("Pythone is"+x)
+#
+# myfunc()
+
+
+# x = 10
+#
+# def mufunc():
+#     global x
+#     x += 5
+#     print(x)
+#
+# mufunc()
+
+# def myfunc():
+#
+#    global x
+#    x -=5
+#    print(x)
+
+# def greet(name):
+#     print("Hello"+name)
+# greet("Abu")
+# greet("Sufian")
+#
+
+# def square(x):
+#     return x**2
+# result=square(5)
+# print(result)
+
+x="awesome"
+def myfunc():
+    global x
+    x="fantastic"
+myfunc()
+print("Pythone is"+x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
