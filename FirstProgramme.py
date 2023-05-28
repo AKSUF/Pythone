36727632fc3563df0b87c8f77914d1bdd3068b31
AKSUF <abu053125@gmail.com>
2023-05-28 21:59:12 +0600

    how to use factorial number

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,11 +1,8 @@
-def fib(n):
-    a=0
-    b=1
-    print(a)
-    print(b)
-    for i in range(2,n):
-        c=a+b
-        a=b
-        b=c
-        print(c)
-fib(1)
+def fact(n):
+    f=1
+    for i in range(1,n+1):
+        f=f*i
+    return f
+x=5
+result=fact(x)
+print(result)
