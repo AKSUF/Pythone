2e03bd472aaeb091057856fe58dcdd36fffe5c45
AKSUF <abu053125@gmail.com>
2023-05-28 14:26:50 +0600

    arrlogspace

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,27 +1,4 @@
-# a=200
-# b=33
-# if b>a:
-#     print("b is greater than a")
-# else:
-#     print("b is not greater than a")
-
-#Checking if an element exists in a list using the in keyword
-# my_list=[1,2,3,4,5]
-# result=2 in my_list
-# print(result)
-# result=6 in my_list
-# print(result)
-x = 5
-y = 10
-z = 15
-
-result = x < y and y < z
-print(result)   # True
-
-result = x < y or y > z
-print(result)   # True
-
-
-
-
+from numpy import *
+arr=logspace(0,16,15)
+print(arr)
 
