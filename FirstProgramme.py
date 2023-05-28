a4a4da98430ed9416dc1c631dd398cb3be04df58
AKSUF <abu053125@gmail.com>
2023-05-28 18:39:54 +0600

    what is function with diffrent uses

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,31 +1,16 @@
 from numpy import *
-arr1=array([
-    [1,2,3,4,5,6],
-    [4,5,6,7,8,9]
-])
-# #to make 2d array to 3d array
-# arr2=arr1.flatten()
-# #make dimensional array
-# arr3=arr1.reshape(3,4)
-# print(arr3)
-# print(arr2)
-# print(arr1.ndim)
-# print(arr1.dtype)
-# print(arr1.shape)
-# print(arr1.size)
-
-n1=matrix('1 2 3; 6 5 6;7 8 9')
-n2=matrix('1 2 3; 6 5 6;7 8 9')
-m=matrix(arr1)
-# print(m)
-# print(n)
-# print(diagonal(n))
-n3=n1*n2
-n4=n1+n2
-print(n3)
-# print(n4)
-
-
+def great():
+    print("Hello")
+    print("Good morning")
+great()
+def add(x,y):
+    c=x+y
+    d=x-y
+    return c,d
+result1,result2=add(5,4)
+# result=add(5,4)
+# print(result)
+print(result1,result2)
 
 
 
