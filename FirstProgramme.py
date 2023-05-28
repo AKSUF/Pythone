d80c3ecd2e58547e1ba72a118a8026d5f969c45c
AKSUF <abu053125@gmail.com>
2023-05-28 21:33:03 +0600

    how to use factorial number

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,7 +1,11 @@
-a=10
-def something():
-    global a
-    a=15
-    print("in fun",a)
-something()
-print("outside",a)
+def fib(n):
+    a=0
+    b=1
+    print(a)
+    print(b)
+    for i in range(2,n):
+        c=a+b
+        a=b
+        b=c
+        print(c)
+fib(1)
