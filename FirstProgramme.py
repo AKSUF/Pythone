acf4b7ff598375680e56102c9b03757715dd933e
AKSUF <abu053125@gmail.com>
2023-05-29 18:05:17 +0600

    how to usw same method in different work

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,18 +1,8 @@
-class Student:
-    def __init__(self,m1,m2):
-        self.m1=m1
-        self.m2=m2
-    def sum(self,a=None,b=None,c=None):
-        s=0
-        if a!=None and b!=None and c!=None:
-         s=a+b+c
-        elif a!=None and b!=None:
-         s=a+b
-        else:
-         a=a
-
-        return s
-
-s1=Student(56,67)
-
-print(s1.sum(5,9))
+class A:
+    def show(self):
+        print("in A Short")
+class B(A):
+    def show(self):
+        print("in B Show")
+a1=B()
+a1.show()
