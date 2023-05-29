84d067173b4019c4635ed4e01c18aae5040f22c6
AKSUF <abu053125@gmail.com>
2023-05-29 06:15:46 +0600

    how to use filter

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,8 +1,11 @@
 
-import  sys
-sys.setrecursionlimit(2000)
-print(sys.getrecursionlimit())
-def great():
-    print("Hello")
-    great()
+# def is_even(n):
+#     return n%2==0
+# name=[3,2,6,4,2,9]
+# events=list(filter(is_even,name))
+# print(events)
 
+
+name=[3,2,6,4,2,9]
+events=list(filter(lambda n:n%2==0,name))
+print(events)
