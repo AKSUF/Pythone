5b361be3b833cea3417c1ed29ac7a74446ca13bd
AKSUF <abu053125@gmail.com>
2023-05-29 06:30:42 +0600

    how to use reduce ,map with lamda expression

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,11 +1,10 @@
+from functools import reduce
 
-# def is_even(n):
-#     return n%2==0
-# name=[3,2,6,4,2,9]
-# events=list(filter(is_even,name))
-# print(events)
-
-
+def add_all(a,b):
+    return a*b
 name=[3,2,6,4,2,9]
 events=list(filter(lambda n:n%2==0,name))
-print(events)
+doubles=list(map(lambda n:n*2,events))
+print(doubles)
+sum=reduce(lambda a,b:a+b,doubles)
+print(sum)
