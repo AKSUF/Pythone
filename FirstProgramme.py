2f4a67f4027f88ec60745f5f98c8c239906f45d9
AKSUF <abu053125@gmail.com>
2023-05-29 15:55:41 +0600

    what is ductype,how use ductype

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,36 +1,16 @@
-class A:
-    def __init__(self):
-        print("in A init")
+class PyChamp:
+    def execute(self):
+        print("Compiling")
+        print("Running")
+class MyEditor:
+    def execute(self):
+        print("spell Check")
+        print("Convenstion check")
 
-    def features1(self):
-        print("Features 1A working")
+class Laptop:
+    def code(self,ide):
+        ide.execute()
 
-    def features2(self):
-        print("Features 2B working")
-
-
-class B(A):
-    def __init__(self):
-        super().__init__()
-        print("in B init")
-
-    def feature3(self):
-        print("Feature 3B working")
-
-    def feature4(self):
-        print("Feature 4B working")
-
-
-class C(B, A):
-    def __init__(self):
-        super().__init__()  # Call B's __init__ method
-        A.__init__(self)  # Call A's __init__ method
-        print("in C init")
-    def feat(self):
-        super().features2()
-
-
-c1 = C()
-c1.features1()
-c1.feat()
-
+ide=PyChamp()
+lap1=Laptop()
+lap1.code(ide)
