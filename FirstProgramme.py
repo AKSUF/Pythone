eaaa8527c3db1ca6aac822a053a8285820274dcd
AKSUF <abu053125@gmail.com>
2023-05-29 20:01:45 +0600

    abstract method in pythone

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,8 +1,21 @@
-class A:
-    def show(self):
-        print("in A Short")
-class B(A):
-    def show(self):
-        print("in B Show")
-a1=B()
-a1.show()
+
+from abc import ABC,abstractmethod
+class Computer(ABC):
+    @abstractmethod
+    def process(self):
+     pass
+class Laptop(Computer):
+  def process(self):
+      print("it's running")
+class Programmer:
+    def work(self,com):
+        print("Solving Bugs")
+class Whiteboard(Computer):
+    def process(self):
+        print("it's running")
+# com=Computer()
+com1=Laptop()
+com2=Whiteboard()
+
+# com.process()
+com1.process()
