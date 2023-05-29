877801bd97ff027db7ecddaffa075233c2f9352b
AKSUF <abu053125@gmail.com>
2023-05-29 11:44:12 +0600

    class variable,function variable

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,26 +1,15 @@
-class Computer:
+class Car:
+    # //class variable
+    wheels=4
+
     def __init__(self):
-        self.name="Navin"
-        self.age=29
-
-
-    def compare(self,other):
-        if self.age==other.age:
-            return True
-        else:
-            return False
-
-
-c1=Computer()
-c2=Computer()
-if c1.compare(c2):
-    print("They are same")
-else:
-    print("They are different")
-
-c1.name="Rashi"
-c1.age=12
-
-print(c1.name)
-print(c2.name)
-
+        # instance variable
+        self.mil=10
+        self.com="BMW"
+c1=Car()
+c2=Car()
+c1.mil=8
+# //class variable
+Car.wheels=5
+print(c1.com,c1.mil,c1.wheels)
+print(c2.com,c2.mil,c2.wheels)
