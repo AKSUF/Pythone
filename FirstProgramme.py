0eff48a82b6f340fc02e2f6807273b4c9c8ccedf
AKSUF <abu053125@gmail.com>
2023-05-29 07:08:29 +0600

    how to use decorator

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,10 +1,19 @@
-from functools import reduce
+# def div(a,b):
+#     if a<b:
+#         a,b=b,a
+#     print(a/b)
+# div(2,4)
 
-def add_all(a,b):
-    return a*b
-name=[3,2,6,4,2,9]
-events=list(filter(lambda n:n%2==0,name))
-doubles=list(map(lambda n:n*2,events))
-print(doubles)
-sum=reduce(lambda a,b:a+b,doubles)
-print(sum)
+def div(a, b):
+    print(a / b)
+# //decorators
+def smart_div(func):
+    def inner(a, b):
+        if a < b:
+            a, b = b, a
+        return func(a, b)
+    return inner
+
+div1 = smart_div(div)
+div1(2, 4)
+
