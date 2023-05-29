fd0111a9e5a704a9f7f149eee791155a5ba7084a
AKSUF <abu053125@gmail.com>
2023-05-29 17:31:35 +0600

    how to work a method in different form

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -2,28 +2,17 @@ class Student:
     def __init__(self,m1,m2):
         self.m1=m1
         self.m2=m2
-    def __add__(self, other):
-        m1=self.m1+other.m1
-        m2=self.m2+other.m2
-        s3=Student(m1,m2)
-        return s3
-    def __gt__(self, other):
-        s1=self.m1+self.m2
-        s2=other.m1+other.m2
-        if s1>s2:
-            return True
+    def sum(self,a=None,b=None,c=None):
+        s=0
+        if a!=None and b!=None and c!=None:
+         s=a+b+c
+        elif a!=None and b!=None:
+         s=a+b
         else:
-            return  False
+         a=a
 
+        return s
 
 s1=Student(56,67)
-s2=Student(60,65)
-s3=s1+s2
 
-if s1>s2:
-    print("s1 wins")
-else:
-    print("s2 wins")
-
-print(s3.m1)
-
+print(s1.sum(5,9))
