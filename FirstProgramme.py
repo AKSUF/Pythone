79e738534f7a400a3baf991bc7fff5a583c9f65e
AKSUF <abu053125@gmail.com>
2023-05-29 13:05:42 +0600

    how to use inner class and method with outer class and object

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,24 +1,23 @@
-
 class Student:
-    school="Telusko"
-    def __init__(self,m1,m2,m3):
-        self.m1=m1
-        self.m2=m2
-        self.m3=m3
-    def avg(self):
-        return (self.m1+self.m2+self.m3)/3
-    @classmethod
-    def info(cls):
-        return cls.school
-    @staticmethod
-    def infostudent():
-        print("This is a Student class in he sgf")
-
-
-s1=Student(34,45,23)
-s2=Student(34,23,50)
-print(s1.avg())
-print(s2.avg())
-print(s1.info())
-Student.info()
-Student.infostudent()
+    def __init__(self,name,rollno):
+        self.name=name
+        self.rollno=rollno
+        self.lap=self.Laptop()
+    def show(self):
+        print(self.name,self.rollno)
+        self.lap.show()
+    class Laptop:
+        def __init__(self):
+            self.brand='HP'
+            self.cpu='i5'
+            self.ram=8
+        def show(self):
+            print(self.brand,self.cpu,self.ram)
+s1=Student("Navin",2)
+s2=Student("Jemmy",3)
+s1.show()
+lap1=s1.lap
+lap2=s1.lap
+print(id(lap1))
+print(id(lap2))
+print(lap1)
