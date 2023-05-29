bc591f7d2d0b4e6c7c16de65e6ebab0a901d7cf9
AKSUF <abu053125@gmail.com>
2023-05-29 13:38:21 +0600

    single,multilevel,multple inheritance

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,23 +1,39 @@
-class Student:
-    def __init__(self,name,rollno):
-        self.name=name
-        self.rollno=rollno
-        self.lap=self.Laptop()
-    def show(self):
-        print(self.name,self.rollno)
-        self.lap.show()
-    class Laptop:
-        def __init__(self):
-            self.brand='HP'
-            self.cpu='i5'
-            self.ram=8
-        def show(self):
-            print(self.brand,self.cpu,self.ram)
-s1=Student("Navin",2)
-s2=Student("Jemmy",3)
-s1.show()
-lap1=s1.lap
-lap2=s1.lap
-print(id(lap1))
-print(id(lap2))
-print(lap1)
+class A:
+    def features1(self):
+        print("Features 1 working")
+    def features2(self):
+        print("Features 2 working")
+class B(A):
+    def feature3(self):
+        print("Feature 3 working")
+    def feature4(self):
+        print("Feature 4 working")
+class C(B):
+    def features5(self):
+        print("Features 5 is working")
+
+    def features6(self):
+        print("Features 6 is working")
+
+class D:
+    def features7(self):
+        print("Features 7 working")
+    def features8(self):
+        print("Features 8 working")
+
+class E(A,D):
+    def features9(self):
+        print("Features 9 working")
+
+    def features10(self):
+        print("Features 10 working")
+a1=A()
+a1.features1()
+a1.features2()
+b1=B()
+b1.features1()
+b1.features2()
+c1=C()
+c1.features1()
+e1=E()
+e1.features1()
