db44092e711bc0c196f6cc548c9084ea12b697ab
AKSUF <abu053125@gmail.com>
2023-05-29 12:26:50 +0600

    different types of method

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,3 +1,4 @@
+
 class Student:
     school="Telusko"
     def __init__(self,m1,m2,m3):
@@ -6,12 +7,18 @@ class Student:
         self.m3=m3
     def avg(self):
         return (self.m1+self.m2+self.m3)/3
-    def get_m1(self):
-        return self.m1
-    def set_m1(self):
-        return self.m1
+    @classmethod
+    def info(cls):
+        return cls.school
+    @staticmethod
+    def infostudent():
+        print("This is a Student class in he sgf")
+
 
 s1=Student(34,45,23)
 s2=Student(34,23,50)
 print(s1.avg())
 print(s2.avg())
+print(s1.info())
+Student.info()
+Student.infostudent()
