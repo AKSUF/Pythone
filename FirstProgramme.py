eb1c8b5b52e08d8871a22efafed280d4fb24d5ff
AKSUF <abu053125@gmail.com>
2023-05-31 12:51:31 +0600

    how to create file,open file,read filr

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,24 +1,5 @@
-def count_words(text):
-    word_counts = {}
 
-    # Split the text into words
-    words = text.split()
-
-    # Count the frequency of each word
-    for word in words:
-        if word in word_counts:
-            word_counts[word] += 1
-        else:
-            word_counts[word] = 1
-
-    return word_counts
-
-
-# Example usage
-text = "This is a sample text to demonstrate word count. This text contains several words, and some words may repeat."
-
-word_counts = count_words(text)
-
-print("Word counts:")
-for word, count in word_counts.items():
-    print(word, ":", count)
+# f=open("G:/Note folder/notefile.txt","x")
+# f.write("Now the file has more content!")
+g = open("G:/Note folder/notefile.txt", "r")
+print(g.read())
