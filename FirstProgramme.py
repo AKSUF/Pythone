b2bd4543e10f27bb812507c3bbec2bb1477080f1
AKSUF <abu053125@gmail.com>
2023-05-31 07:07:56 +0600

    how to use

--- FirstProgramme.py
+++ FirstProgramme.py
@@ -1,11 +1,27 @@
-def calculate_average_temperature(temperatures):
-    total_temperature = sum(temperatures)
-    num_readings = len(temperatures)
-    average_temperature = total_temperature / num_readings
-    return average_temperature
+def analyze_text(text):
+    words=text.split()
+    total_words=len(words)
+    unique_words=len(set(words))
+    total_characters=sum(len(word) for word in words)
+    average_word_length=total_characters/total_words
 
-river_temperatures = [12.5, 13.2, 14.8, 15.6, 13.9, 12.1, 11.8, 14.3]
-average_temp = calculate_average_temperature(river_temperatures)
+    word_frequency={}
+    for word in words:
+        if word in word_frequency:
+            word_frequency[word]+=1
+        else:
+            word_frequency[word]=1
 
-# Display the average temperature
-print("Average River Temperature: {:.2f} degrees".format(average_temp))
+    analysis_result={
+        "total_words":total_words,
+        "unique_words":unique_words,
+        "average_word_length":average_word_length,
+        "word_frequency":word_frequency
+    }
+    return analysis_result
+text="This is a sample txt.Itcontains some words.Sample txt is used for analysis"
+result=analyze_text(text)
+print("Total Words",result["total_words"])
+print("Unique Words",result["unique_words"])
+print("Average Word Length",result["average_word_length"])
+print("Word Frequency",result["word_frequency"])
