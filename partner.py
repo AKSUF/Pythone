5e240f822729ffa5f572318b0c2670fd15c81363
AKSUF <abu053125@gmail.com>
2023-07-20 23:09:32 +0600

    how to use

--- partner.py
+++ partner.py
@@ -1,20 +1,18 @@
 import matplotlib.pyplot as plt
-import numpy as np
-print(plt.style.available)
-plt.style.use('fivethirtyeight')
-dev_x=[25,26,27,28,29,30,31,32,33,34,35]
-dev_y=[45331,75332,95333,75334,65335,75336,85337,75338,35339,25340,75341]
-plt.plot(dev_x,dev_y,color='k',linestyle='--',marker='o',label="All Devs")
 
-py_dev_x=[25,26,27,28,29,30,31,32,33,34,35]
-py_dev_y=[456541,456542,456543,456544,456545,456546,456547,456548,456549,456550,456551]
-plt.plot(dev_x,py_dev_y,color='b',marker='o',label="Python")
-js_dev_y=[156541,256542,356543,956544,156545,356546,856547,356548,756549,956550,356551]
-plt.plot(dev_x,js_dev_y,color='r',marker='o',label="Javascript")
+dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+dev_y = [45331, 75332, 95333, 75334, 65335, 75336, 85337, 75338, 35339, 25340, 75341]
+
+# Plot the bar chart
+plt.bar(dev_x, dev_y, color='k', linestyle='--', label="All Devs")
+
+# Add markers on top of the bars
+# plt.plot(dev_x, dev_y, marker='o', markersize=5, color='red')
+
 plt.xlabel("Ages")
 plt.ylabel("Median Salary (USD)")
 plt.title('Media Salary (USD) by Age')
-plt.legend(["All Dev's","Python","Javascript"])
+plt.legend(["All Dev's", "Markers on Bars"])
 plt.grid(True)
 plt.tight_layout()
 plt.savefig('diagram.png')
