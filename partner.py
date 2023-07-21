2aad89d200f08c2f1bd264f56d976939dc119257
AKSUF <abu053125@gmail.com>
2023-07-21 09:24:55 +0600

    how to use

--- partner.py
+++ partner.py
@@ -8,19 +8,17 @@ with open('G:\File store\data.csv') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     language_counter = Counter()
     for row in csv_reader:
-        languages = row['LanguagesWorkedWith'].split(':')
-        language_counter.update(languages)
-
+        language_counter.update(row['LanguagesWorkedWith'].split(';'));
 languages = list(language_counter.keys())
 popularity = list(language_counter.values())
 
 plt.bar(languages, popularity)
 
 plt.xlabel("Most popular programming languages")
-plt.ylabel("Popularity")
+# plt.ylabel("Popularity")
 plt.title('Most Popular Programming Languages')
 plt.xticks(rotation=90)
-# plt.tight_layout()
+plt.tight_layout()
 # plt.savefig('diagram.png')
 plt.show()
 
